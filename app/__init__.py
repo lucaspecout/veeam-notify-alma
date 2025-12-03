292230172a0b3b38ce32f54af1ce367905fd2708
Lucas Pecout <100285952+lucaspecout@users.noreply.github.com>
2025-12-03 11:57:47 +0100

    Configure template and static directories

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,9 @@ db = SQLAlchemy()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__, template_folder="../templates", static_folder="../static"
+    )
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///app.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret-key")
